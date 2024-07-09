2ae9c50300c2972915b7b0a787bb91a79126dd5c
k1LoW <k1lowxb@gmail.com>
2024-07-10 00:21:44 +0900

    Fix lint warn

--- waitmap.go
+++ waitmap.go
@@ -42,11 +42,6 @@ func (m *waitMap[K, V]) Get(k K) V {
 func (m *waitMap[K, V]) TryGet(k K) (V, bool) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	lock, ok := m.lockmap[k]
-	if !ok {
-		lock = sync.NewCond(&m.mu)
-		m.lockmap[k] = lock
-	}
 	v, ok := m.valmap[k]
 	return v, ok
 }
